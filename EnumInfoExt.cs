41b6b2bfa75c466aa338326231fa0353bbcfaac0
WarmasterPrime <warmasterprime.gamming@gmail.com>
2023-12-11 12:47:13 -1000

    - Removed unused usings.

--- EnumInfoExt.cs
+++ EnumInfoExt.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VEnum
+﻿namespace VEnum
 {
 	/// <summary>
 	/// Provides information related to an <see cref="Enum"/>.
